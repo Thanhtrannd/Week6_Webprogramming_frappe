516a5d2fb33491be18cd3bc320654cd72e3a91fe
Thanh Tran <tndt.1902@gmail.com>
2022-10-15 12:37:42 +0300

    Implement post api fetch function

---
+++ src/application.js
@@ -0,0 +1,76 @@
+const URL = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"
+
+const jsonQuery = {
+    "query": [
+        {
+            "code": "Vuosi",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "2000",
+                    "2001",
+                    "2002",
+                    "2003",
+                    "2004",
+                    "2005",
+                    "2006",
+                    "2007",
+                    "2008",
+                    "2009",
+                    "2010",
+                    "2011",
+                    "2012",
+                    "2013",
+                    "2014",
+                    "2015",
+                    "2016",
+                    "2017",
+                    "2018",
+                    "2019",
+                    "2020",
+                    "2021"
+                ]
+            }
+        },
+        {
+            "code": "Alue",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "SSS"
+                ]
+            }
+        },
+        {
+            "code": "Tiedot",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "vaesto"
+                ]
+            }
+        }
+    ],
+    "response": {
+        "format": "json-stat2"
+    }
+}
+
+
+const getData = async () => {
+    const url = URL
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(jsonQuery)
+    })
+    if (!res.ok) {
+        return;
+    }
+    const data = await res.json()
+
+    return data
+}
+
+data = getData()
+console.log(data)
