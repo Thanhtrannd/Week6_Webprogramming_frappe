ec0f2ab78aa6f4458e3a64a7e6ebc3337a53b50f
Thanh Tran <tndt.1902@gmail.com>
2022-10-15 18:06:45 +0300

    Add prediction functionality and build chart accordingly

--- src/application.js
+++ src/application.js
@@ -79,10 +79,6 @@ const getData = async () => {
   }
   const data = await res.json();
 
-  return data;
-};
-
-const buildChart = (data) => {
   const years = Object.values(data.dimension.Vuosi.category.label);
   const aluet = Object.values(data.dimension.Alue.category.label);
   const labels = Object.values(data.dimension.Vuosi.category.label);
@@ -92,48 +88,55 @@ const buildChart = (data) => {
   let nAluet = aluet.length;
   let nYears = years.length;
 
-  let dataset = [];
+  dataset = {
+    years: years,
+    data: []
+  };
   aluet.forEach((alue, idx) => {
     let aluePopu = []
     for (let year = 0; year < nYears; year++) {
       aluePopu.push(population[year * nAluet + idx])
     }
-    dataset[idx] = {
+    dataset.data[idx] = {
       name: alue,
       values: aluePopu
     }
   })
   console.log(dataset)
 
+  return dataset;
+};
+
+const buildChart = (dataset) => {
+  let nYears = dataset.years.length;
+
   const chartData = {
-    labels: years,
-    datasets: dataset
+    labels: dataset.years,
+    datasets: dataset.data
   }
 
   const chart = new frappe.Chart("#chart", {
-    title: 'Finnish population from ' + years[0] +  ' to ' + years[nYears-1],
+    title: 'Finnish population from ' + dataset.years[0] +  ' to ' + dataset.years[nYears-1],
     data: chartData,
     type: "line",
     colors: ['#eb5146'],
     height: 450
   })
-
-
 }
 
 const submitButton = document.getElementById("submit-data");
 const inputArea = document.getElementById("input-area");
+const addPrediction = document.getElementById("add-data");
 
-
-const main = async () => {
-  const data = await getData();
-  console.log(data);
-  buildChart(data)
+const reloadchart = async () => {
+  dataset = await getData();
+  console.log(dataset);
+  buildChart(dataset)
 };
 
 const {areaName, areaCode} = getAreaCode()
 //console.log(areaName)
-main();
+reloadchart();
 
 const addArea = (code) => {
   if (!jsonQuery.query[1].selection.values.includes(code)) {
@@ -165,6 +168,30 @@ submitButton.addEventListener("click", async function () {
   console.log('code is '+ code)
 
   addArea(code)
-  main();
+  reloadchart();
 })
 
+addPrediction.addEventListener("click", () => {
+  let nYears = dataset.years.length;
+  dataset.years.push((Number(dataset.years[nYears-1])+1).toString())
+  dataset.data.forEach((alue, alueIdx) => {
+    let meanDelta = 0;
+    dataset.data[alueIdx].values.forEach((point, point_Idx) => {
+      point = Number(point)
+      next_point = Number(dataset.data[alueIdx].values[point_Idx + 1])
+      if (point_Idx == nYears-1) {
+        meanDelta = meanDelta / (nYears-1) + point;
+      }
+      else {
+        meanDelta = meanDelta + (next_point - point);
+      }
+      console.log(meanDelta)
+    })
+    dataset.data[alueIdx].values.push(meanDelta)
+    console.log(meanDelta)
+    console.log(dataset.data[alueIdx].values) 
+
+  })
+  console.log(dataset)
+  buildChart(dataset)
+})
